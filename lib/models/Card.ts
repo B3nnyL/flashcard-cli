ab0f7940e15a89c88e10bf4d05adc92c0257ecff
mot0laura <mot0laura@gmail.com>
2018-12-19 14:38:06 +1100

    [feat] add Card and Stack classes

---
+++ lib/models/Card.ts
@@ -0,0 +1,37 @@
+import Content from '../interfaces/Content'
+import { v4 as uuid } from "uuid";
+export default class Card {
+    _id: string;
+    createdTime: Date;
+    content: Content;
+    familarity: number;
+
+    constructor(_content:Content, _familarity = 1) {
+        this._id = uuid();
+        this.createdTime = new Date();
+        this.content = _content
+        this.familarity = _familarity;
+    }
+
+    getContentFront(): string {
+        return this.content.content__front;
+    }
+
+    getContentBack(): string {
+        return this.content.content__back;
+    }
+
+    getFamilarity(): string {
+        return this.familarity.toString();
+    }
+
+    setFamilarity(newFamilarity) {
+        this.familarity = newFamilarity;
+    }
+
+    setContent(newContent:Content) {
+        this.content.content__front = newContent.content__front;
+        this.content.content__back = newContent.content__back;
+    }
+
+}
---
+++ lib/models/Stack.ts
@@ -0,0 +1,47 @@
+import StackMeta from '../interfaces/StackMeta';
+import Card from '../models/Card';
+export default class Stack {
+    meta: StackMeta;
+    cards?: Array<Card>;
+
+    constructor(_name: string, _author?: string, _cards?: Array<Card>) {
+        const date = new Date();
+        this.meta = {
+            name: _name,
+            createdTime: date,
+            author: _author
+        }
+        if(_cards) this.cards = _cards
+    }
+
+    getCards(): Array<Card> {
+        if(this.cards) return this.cards
+    }
+
+    setCards(_cards: Array<Card>) {
+        this.cards = _cards
+    }
+
+    getName(): string {
+        return this.meta.name
+    }
+
+    setMeta(_name: string, _author?: string) {
+        this.meta.name = _name
+        if(_author) this.meta.author = _author
+    }
+
+    getCard(id: string): Card {
+        const existingCards = this.getCards();
+        return existingCards.filter(card => card._id === id)[0];
+    }
+
+    addCard(_card: Card) {
+        const existingCards = this.getCards();
+        return existingCards.push(_card);
+    }
+
+    removeCard(id: string) {
+        this.cards = this.getCards().filter( card => card._id !== id);
+    }
+}
